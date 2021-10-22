6f2097cd3f2eb77f113529af6dd0fc7f4b95005c
allldo <dotan2.maks@mail.ru>
2021-10-22 11:49:16 +0600

    changes

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from landing.models import Service
+from landing.models import FAQ, Service
 
 # Create your views here.
 
@@ -8,4 +8,5 @@ from landing.models import Service
 def main_page(request):
     context = {}
     context['Service'] = Service.objects.all()
+    context['FAQ'] = FAQ.objects.all()
     return render(request, 'landing/main_page.html', context)
