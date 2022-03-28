47cb8e712fa814e11582d2dc2d7e799e9894e663
dev13 <dev13@zavode.ru>
2022-03-28 15:57:52 +0600

    qqe

--- ecology_full_new/urls.py
+++ ecology_full_new/urls.py
@@ -1,13 +1,12 @@
-
-from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from . import  settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(("landing.urls", 'landing'), namespace='landing')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
