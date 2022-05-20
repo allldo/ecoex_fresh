2d2f60f7f602e9e1f4f3ceabd7282398f25e7404
allldo <dotan2.maks@mail.ru>
2022-05-20 20:30:12 +0600

    urls

--- ecology_full_new/urls.py
+++ ecology_full_new/urls.py
@@ -6,11 +6,16 @@ from . import  settings
 handler404 = 'landing.views.handler404'
 handler500 = 'landing.views.handler500'
 handler403 = 'landing.views.handler403'
+if settings.DEBUG:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include(("landing.urls", 'landing'), namespace='landing')),
+        path('tinymce/', include('tinymce.urls')),
+    ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(("landing.urls", 'landing'), namespace='landing')),
-
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
-urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+        path('admin/', admin.site.urls),
+        path('', include(("landing.urls", 'landing'), namespace='landing')),
+        path('tinymce/', include('tinymce.urls')),
+    ]
