cee0ffb03e2cbf456381d608999e62e42e275c22
allldo <dotan2.maks@mail.ru>
2022-05-30 14:38:06 +0600

    model

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,9 @@ class Project(models.Model):
     description_in_front = models.CharField(max_length=120)
     price = models.CharField(max_length=15)
     image = models.ImageField(upload_to='project/')
+
+
+class City(models.Model):
+    name = models.CharField(max_length=155)
+    aqi_us = models.IntegerField
+    main_pollutant_us = models.CharField(max_length=15)
